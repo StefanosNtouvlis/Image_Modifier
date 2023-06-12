cf8052e3d069d39911e22925bc0b827ae82b4192
Stefanos Ntouvlis <stefanos.ntouvlis@gmail.com>
2023-06-13 01:58:05 +0300

    FIles

---
+++ photoEditor.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = "./imgs"
+pathOut = "/editedImgs"
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    # img_gray = img.convert("L")
+    # img_gray_smooth = img_gray.filter(ImageFilter.SMOOTH)
+    # edges_smooth = img_gray_smooth.filter(ImageFilter.FIND_EDGES)
+
+    sharpened = img.filter(ImageFilter.SHARPEN)
+
+    enhancer = ImageEnhance.Contrast(sharpened)
+    enhanced = enhancer.enhance(factor=1.5)
+
+    brightener = ImageEnhance.Brightness(enhanced)
+    brightened = brightener.enhance(factor=0.8)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    brightened.save(f".{pathOut}/{clean_name}_edited.jpg", dpi=(300, 300))
+
+# Credits to Internet Made Coder for the inspiration.
